04c9be3231fb635419958a76af74b59df419b48d
waraneymmbu <waraney.maikel@ti.ukdw.ac.id>
2025-02-09 23:38:26 +0800

    Update compoundingInterest.py

--- compoundingInterest.py
+++ compoundingInterest.py
@@ -1,1 +1,10 @@
+import math
 
+P = 200_000_000 
+A = 400_000_000  
+r = 0.10        
+n = 12      
+
+t = math.log(A / P) / (n * math.log(1 + (r / n)))
+
+print(f"Waktu yang dibutuhkan agar uang Erika menjadi Rp 400 juta: {t:.2f} tahun")
